24e1e79d10984efe951f2be75b2c3126c61bc394
Mila <milafazekas1999@gmail.com>
2024-06-04 19:34:25 +0200

    fix scroll issues

--- script/script.js
+++ script/script.js
@@ -25,27 +25,34 @@ let kategorije = [
   "NonAlchoholic",
 ];
 function myFunction(name) {
-    var imeKategorije = document.getElementById(name); 
-    var meniKategorije = document.getElementById(name + "A"); 
-    
-    kategorije.filter(function (item) {
-      return item !== name;
-    });
-  
+    var imeKategorije = document.getElementById(name);
+    var meniKategorije = document.getElementById(name + "A");
+
     kategorije.forEach((element) => {
-      let neselektovanaKategorija = document.getElementById(element); 
-      neselektovanaKategorija.style.backgroundColor = "#11ffee00";
-      neselektovanaKategorija.style.opacity = "1";
-  
-      let selektovanaKategorija = document.getElementById(element + "A"); 
-      selektovanaKategorija.style.backgroundColor = "#95D8EA"; 
-      selektovanaKategorija.style.color = "black";
+        let neselektovanaKategorija = document.getElementById(element);
+        if (neselektovanaKategorija) {
+            neselektovanaKategorija.style.backgroundColor = "#11ffee00";
+            neselektovanaKategorija.style.opacity = "1";
+        }
+
+        let selektovanaKategorija = document.getElementById(element + "A");
+        if (selektovanaKategorija) {
+            selektovanaKategorija.style.backgroundColor = "#95D8EA";
+            selektovanaKategorija.style.color = "black";
+        }
     });
-  
-    imeKategorije.style.backgroundColor = "none"; 
-    imeKategorije.style.opacity = "1";
-    meniKategorije.style.backgroundColor = "black";
-    meniKategorije.style.color = "white";
- 
-    imeKategorije.scrollIntoView({ behavior: "smooth" });
-  }
+
+    if (imeKategorije) {
+        imeKategorije.style.backgroundColor = "none";
+        imeKategorije.style.opacity = "1";
+        imeKategorije.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+
+    if (meniKategorije) {
+        meniKategorije.style.backgroundColor = "black";
+        meniKategorije.style.color = "white";
+
+        // Skrolujemo tako da kliknuta meni kategorija bude na vrhu roditeljskog elementa
+        meniKategorije.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+}

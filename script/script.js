073583bd673a4f2596b44e010469abcaf6ac11f8
Mila <milafazekas1999@gmail.com>
2024-06-05 11:59:46 +0200

    Fix design

--- script/script.js
+++ script/script.js
@@ -1,59 +1,65 @@
 let kategorije = [
-  "Kafe",
-  "PrirodniSokovi",
-  "BezalkoholnaPica",
-  "Vode",
-  "GaziraniSokovi",
-  "EnergetskiNapici",
-  "Piva",
-  "Tocenopivo",
-  "Vodke",
-  "Likeri",
-  "Tekile",
-  "Konjak",
-  "Gin",
-  "Viski",
-  "Piva",
-  "Cideri",
-  "Rakija",
-  "VinaC",
-  "VinaF",
-  "LetnjiMix",
-  "Pizza",
-  "Sendvici",
-  "Kokteli",
-  "NonAlchoholic",
-];
-function myFunction(name) {
-    var imeKategorije = document.getElementById(name);
-    var meniKategorije = document.getElementById(name + "A");
-
-    kategorije.forEach((element) => {
-        let neselektovanaKategorija = document.getElementById(element);
-        if (neselektovanaKategorija) {
-            neselektovanaKategorija.style.backgroundColor = "#11ffee00";
-            neselektovanaKategorija.style.opacity = "1";
-        }
-
-        let selektovanaKategorija = document.getElementById(element + "A");
-        if (selektovanaKategorija) {
-            selektovanaKategorija.style.backgroundColor = "#95D8EA";
-            selektovanaKategorija.style.color = "black";
-        }
-    });
-
-    if (imeKategorije) {
-        imeKategorije.style.backgroundColor = "none";
-        imeKategorije.style.opacity = "1";
-        imeKategorije.scrollIntoView({ behavior: "smooth", block: "start",inline: "start" });
-    }
-
-    if (meniKategorije) {
-        meniKategorije.style.backgroundColor = "black";
-        meniKategorije.style.color = "white";
-        setTimeout(function() {
-            meniKategorije.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
-        }, 100); 
-    }
-
-}
+    "Kafe",
+    "PrirodniSokovi",
+    "BezalkoholnaPica",
+    "Vode",
+    "GaziraniSokovi",
+    "EnergetskiNapici",
+    "Piva",
+    "Tocenopivo",
+    "Vodke",
+    "Likeri",
+    "Tekile",
+    "Konjak",
+    "Gin",
+    "Viski",
+    "Piva",
+    "Cideri",
+    "Rakija",
+    "VinaC",
+    "VinaF",
+    "LetnjiMix",
+    "Pizza",
+    "Sendvici",
+    "Kokteli",
+    "NonAlchoholic",
+  ];
+  
+  function myFunction(name) {
+      var imeKategorije = document.getElementById(name);
+      var meniKategorije = document.getElementById(name + "A");
+  
+      kategorije.forEach((element) => {
+          let neselektovanaKategorija = document.getElementById(element);
+          if (neselektovanaKategorija) {
+              neselektovanaKategorija.style.backgroundColor = "#11ffee00";
+              neselektovanaKategorija.style.opacity = "1";
+          }
+  
+          let selektovanaKategorija = document.getElementById(element + "A");
+          if (selektovanaKategorija) {
+              selektovanaKategorija.style.backgroundColor = "#95D8EA";
+              selektovanaKategorija.style.color = "black";
+          }
+      });
+  
+      if (imeKategorije) {
+          imeKategorije.style.backgroundColor = "none";
+          imeKategorije.style.opacity = "1";
+          imeKategorije.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
+      }
+  
+      if (meniKategorije) {
+          meniKategorije.style.backgroundColor = "black";
+          meniKategorije.style.color = "white";
+      }
+  }
+  
+  // Add scroll event listeners to synchronize scrolling
+  document.getElementById('meniKategorije').addEventListener('scroll', function() {
+      document.getElementById('imeKategorije').scrollTop = this.scrollTop;
+  });
+  
+  document.getElementById('imeKategorije').addEventListener('scroll', function() {
+      document.getElementById('meniKategorije').scrollTop = this.scrollTop;
+  });

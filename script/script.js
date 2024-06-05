a5f770a432b1a717099f20f6a7d0bbef4633147a
Svetozar Ivanovic <svetozar.ivanovic@noubis.com>
2024-06-05 18:04:56 +0200

    Connected scrolling between menu and content categories

--- script/script.js
+++ script/script.js
@@ -25,34 +25,67 @@ let kategorije = [
     "NonAlchoholic",
   ];
   
+  var parentMenu = document.getElementById("menuContainer");
+  var parentCategory = document.getElementById("contentCategory");
+  var arrayOfItems = [];
+  var arrayOfMenus = [];
+  
+  kategorije.forEach((item) => {
+    var meniTempKategorija = document.getElementById(item + "A");
+    var tempKategorija = document.getElementById(item);
+    arrayOfItems.push(tempKategorija);
+    arrayOfMenus.push(meniTempKategorija);
+  });
+  
+  parentMenu.addEventListener("scroll", () => {
+    const scrollPosition = parentMenu.scrollLeft;
+    arrayOfMenus.forEach((item, index) => {
+      const itemOffset = item.offsetLeft;
+      const itemWidth = item.offsetWidth;
+  
+      if (
+        scrollPosition >= itemOffset &&
+        scrollPosition < itemOffset + itemWidth
+      ) {
+
+        parentCategory.scrollTo({
+          top: arrayOfItems[index].offsetTop - parentCategory.offsetTop,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+  
   function myFunction(name) {
-      var imeKategorije = document.getElementById(name);
-      var meniKategorije = document.getElementById(name + "A");
+    var imeKategorije = document.getElementById(name);
+    var meniKategorije = document.getElementById(name + "A");
   
-      kategorije.forEach((element) => {
-          let neselektovanaKategorija = document.getElementById(element);
-          if (neselektovanaKategorija) {
-              neselektovanaKategorija.style.backgroundColor = "#11ffee00";
-              neselektovanaKategorija.style.opacity = "1";
-          }
+    kategorije.forEach((element) => {
+      let neselektovanaKategorija = document.getElementById(element);
+      if (neselektovanaKategorija) {
+        neselektovanaKategorija.style.backgroundColor = "#11ffee00";
+        neselektovanaKategorija.style.opacity = "1";
+      }
   
-          let selektovanaKategorija = document.getElementById(element + "A");
-          if (selektovanaKategorija) {
-              selektovanaKategorija.style.backgroundColor = "#95D8EA";
-              selektovanaKategorija.style.color = "black";
-          }
+      let selektovanaKategorija = document.getElementById(element + "A");
+      if (selektovanaKategorija) {
+        selektovanaKategorija.style.backgroundColor = "#95D8EA";
+        selektovanaKategorija.style.color = "black";
+      }
+    });
+  
+    if (imeKategorije) {
+      imeKategorije.style.backgroundColor = "none";
+      imeKategorije.style.opacity = "1";
+  
+      parentCategory.scrollTo({
+        top: imeKategorije.offsetTop - parentCategory.offsetTop,
+        behavior: "smooth",
       });
+    }
   
-      if (imeKategorije) {
-          imeKategorije.style.backgroundColor = "none";
-          imeKategorije.style.opacity = "1";
-          imeKategorije.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
-      }
-  
-      if (meniKategorije) {
-          meniKategorije.style.backgroundColor = "black";
-          meniKategorije.style.color = "white";
-      }
+    if (meniKategorije) {
+      meniKategorije.style.backgroundColor = "black";
+      meniKategorije.style.color = "white";
+    }
   }
-  
-  
